6d758cd54f55d9e5870a5a079d29ecf31aa1b6bc
Akshita Pathak <pathakshita07@gmail.com>
2026-02-15 13:34:48 +0530

    Day 10 part1

---
+++ src/Day10/AccessingPackages.java
@@ -0,0 +1,11 @@
+package Day10;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class AccessingPackages {
+    public static void main(String[] args) {
+        Date date;
+        ArrayList list;
+    }
+}
